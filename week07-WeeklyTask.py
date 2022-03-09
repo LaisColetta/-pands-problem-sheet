0700f45b34abf6c85abb95f42477fc496396238e
LaisColetta <laiscoletta@gmail.com>
2022-03-09 13:42:43 +0000

    week07

--- week07-WeeklyTask.py
+++ week07-WeeklyTask.py
@@ -1,16 +1,22 @@
-# Program to get letter count in a text file
- 
-# explicit function to return the letter count
+#Program to count letter 'e' in a text file
+#Author: Lais
+#Reference: www.geeksforgeeks.org,https://docs.python.org/3/library/functions.html#open, https://www.w3resource.com/python-exercises/basic/python-basic-1-exercise-7.php
+
+# We start by creating a function that reads the file:
 def letterFrequency(fileName, letter):
     # open file in read mode
-    file = open(fileName, 'e')
- 
+    file = open(fileName, "r")
     # store content of the file in a variable
     text = file.read()
- 
-    # using count()
-    return text.count(letter)
- 
- 
-# call the function and display the letter count
-print(letterFrequency('gfg.txt', 'e'))
+    #count variable (as we did in Lab07.02)
+    count = 0
+    for character in text:
+        #check if character contains the given letter
+        if character == letter:
+            count += 1
+            
+    # return letter count
+    return count
+
+# call function and display the letter count
+print(letterFrequency('week06-WeeklyTask.py', 'e'))
