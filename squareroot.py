590a22e6aebad26df37da4e5fd35b1d6652700bd
LaisColetta <laiscoletta@gmail.com>
2022-04-06 20:17:17 +0100

    problem_sheet

--- squareroot.py
+++ squareroot.py
@@ -7,20 +7,21 @@ def sqrt(num, error=0.00001):
     # variable for the first guess = num
     guess = num
     #calculate a difference that is going to be used to measure against the error rate paramater set previously.
-    diff = 999999
+    diff2 = 999999
     #while loop to keep repeting formula and improving our guess until it meets the level of error level we set.
-    while diff>error:
+    while diff2>error:
         #newton's method formula:
         newGuess = guess - ((guess**2 - num)/ (2* guess))
-        #calculate the error. use ABS funtion to take the absolute value in case it is a negative value. 
-        diff = abs(newGuess - guess)
+        #calculate the error. use ABS function to take the absolute value in case it is a negative value. 
+        diff1 = (newGuess - guess)
+        diff2 = abs(diff1)
         
     #If the different is still too big (bigger than 'error') the loop will continue, otherwise it ends.
         guess = newGuess
     return guess
 
 a = (input('Please enter a positive number: '))
-b = sqrt(a)
+b = sqrt(float(a))
 print ('The square root of {} is approx. {}'.format(a,b))
 
 
